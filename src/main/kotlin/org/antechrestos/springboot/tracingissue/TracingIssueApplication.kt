8263d3e988e0b9fc28c40040a33c28c93b06ffc7
Ben Einaudi <antechrestos@gmail.com>
2024-01-24 10:45:05 +0100

    fixes tracing issue with CoWebFilter

--- src/main/kotlin/org/antechrestos/springboot/tracingissue/TracingIssueApplication.kt
+++ src/main/kotlin/org/antechrestos/springboot/tracingissue/TracingIssueApplication.kt
@@ -1,7 +1,10 @@
 package org.antechrestos.springboot.tracingissue
 
+import io.micrometer.core.instrument.kotlin.asContextElement
+import io.micrometer.observation.ObservationRegistry
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import kotlinx.coroutines.withContext
 import org.antechrestos.springboot.tracingissue.infrastructure.UserRepositoryMongoStore
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.info.BuildProperties
@@ -9,22 +12,34 @@ import org.springframework.boot.info.GitProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
+import org.springframework.web.server.CoWebFilter
+import org.springframework.web.server.CoWebFilterChain
+import org.springframework.web.server.ServerWebExchange
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = [UserRepositoryMongoStore::class])
 class TracingIssueApplication {
-	@Bean
-	fun publicApi(
-		buildProperties: BuildProperties?,
-		gitProperties: GitProperties?,
-	): OpenAPI = OpenAPI()
-		.info(
-			Info()
-				.title("Demo Tracing Issue")
-				.version("latest-SNAPSHOT")
-		)
+    @Bean
+    fun publicApi(
+        buildProperties: BuildProperties?,
+        gitProperties: GitProperties?,
+    ): OpenAPI = OpenAPI()
+        .info(
+            Info()
+                .title("Demo Tracing Issue")
+                .version("latest-SNAPSHOT")
+        )
+
+    @Bean
+    fun coroutineContextFilter(observationRegistry: ObservationRegistry): CoWebFilter = object : CoWebFilter() {
+        override suspend fun filter(exchange: ServerWebExchange, chain: CoWebFilterChain) {
+            withContext(observationRegistry.asContextElement()){
+                chain.filter(exchange)
+            }
+        }
+    }
 }
 
 fun main(args: Array<String>) {
-	runApplication<TracingIssueApplication>(*args)
+    runApplication<TracingIssueApplication>(*args)
 }
